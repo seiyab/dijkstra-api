339ed17e8e9b10dd82b9a4ca077436b170b628bf
seiya <hoge@hoge>
2017-11-26 14:36:48 +0900

    コントローラーを実装

--- src/main/java/com/example/api/application/controller/TaskController.java
+++ src/main/java/com/example/api/application/controller/TaskController.java
@@ -3,21 +3,21 @@ package com.example.api.application.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.api.domain.model.Label;
-import com.example.api.domain.repository.LabelRepository;
+import com.example.api.domain.service.TaskService;
+import com.example.api.domain.service.response.TaskResponse;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
   @Autowired
-  LabelRepository labelRepository;
+  TaskService taskService;
 
-  @GetMapping("/{algorithmId}")
-  public List<Label> tasks(@PathVariable Integer algorithmId) {
-    return labelRepository.findByAlgorithm_AlgorithmId(algorithmId);
+  @GetMapping("")
+  public List<TaskResponse> tasks(@RequestParam("algorithm_id") Integer algorithmId) {
+    return taskService.findTasksByAlgorithmId(algorithmId);
   }
 }
