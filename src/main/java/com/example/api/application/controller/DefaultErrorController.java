75dfe4de6ef1b6bc9e5d66cc5bbf7e65ae1c4a9f
seiya <hoge@hoge>
2017-11-28 21:19:37 +0900

    標準のエラー時の応答

---
+++ src/main/java/com/example/api/application/controller/DefaultErrorController.java
@@ -0,0 +1,25 @@
+package com.example.api.application.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultErrorController implements ErrorController {
+  private final static String PATH = "/error";
+
+  @RequestMapping("/error")
+  public Map<String, String> error() {
+    HashMap<String, String> responseBody = new HashMap<>();
+    responseBody.put("message", "Invalid Request.");
+    return responseBody;
+  }
+
+  @Override
+  public String getErrorPath() {
+    return PATH;
+  }
+
+}
